c993feb7e348442f52c7ce52d13dd860985fa62c
Kalhaar Savaj <kalhaar@google.com>
2026-02-15 16:51:45 -0500

    adding files

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "project_id" {
   type        = string
+  default     = "kalhaarsavaj"
 }
 
 variable "region" {
   type    = string
+  default = "us-central1"
 }
 
 variable "tf_state_bucket" {
   type    = string
+  default = "kalhaarsavaj-tf-state"
 }
 
 variable "infra_bucket" {
   type    = string
+  default = "kalhaarsavaj-infra-bucket"
 }
